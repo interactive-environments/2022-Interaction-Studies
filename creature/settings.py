44724db8cbd56cd508939ed8ac2c7cf23b56506f
Aadjan van der Helm <a.j.c.vanderhelm@tudelft.nl>
2022-08-28 19:41:30 +0200

    Removing password

--- creature/settings.py
+++ creature/settings.py
@@ -1,8 +1,6 @@
 settings = {
 #    "ssid": "TUD-facility",
-    "ssid": "Yamanaka",
 #    "password": "63063638471e4",
-    "password": "waggle24[wanting",
     "broker": "greatinteractivereef.cloud.shiftr.io",
     "user": "greatinteractivereef",
     "token": "YbOk0JfBm8dtVkW3",

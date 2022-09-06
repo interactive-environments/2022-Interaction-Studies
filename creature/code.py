4aad3c373f1a931cc4c610f08456bf0603c4d1a5
cKrijgsman <c.j.krijgsman@student.tudelft.nl>
2022-09-07 00:44:57 +0200

    select test behaviour as default

--- creature/code.py
+++ creature/code.py
@@ -1,7 +1,7 @@
 # Chose one of the following imports to start the correct program.
 
 # uncommenting this line will run the online interactive reef code.
-import reef
+# import reef
 
 # uncommenting this line will run the local behaviour test.
 import behaviour_test

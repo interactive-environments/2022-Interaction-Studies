39f05851782662e8d261177b8d51dadd9622ca85
Aadjan van der Helm <a.j.c.vanderhelm@tudelft.nl>
2022-09-07 16:26:00 +0200

    Move button to pin A0

--- creature/lib/components/button.py
+++ creature/lib/components/button.py
@@ -5,20 +5,20 @@ import digitalio
 
 class Button():
     previous_button_value = False
-    
+
     def __init__(self):
-        self.button = digitalio.DigitalInOut(board.D2)
+        self.button = digitalio.DigitalInOut(board.A0)
         self.button.direction = digitalio.Direction.INPUT
 
 
     def sense(self):
         current_button_value = self.button.value
         button_status = False
-        
+
         # Only trigger on button release
         if (self.previous_button_value == True and current_button_value == False):
             button_status = True
 
         self.previous_button_value = current_button_value
-        
+
         return button_status

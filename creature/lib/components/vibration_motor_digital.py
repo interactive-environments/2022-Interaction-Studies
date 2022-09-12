c8d1fa4dd83883dc86da2a97715ac7ff8dd86058
Aadjan van der Helm <a.j.c.vanderhelm@tudelft.nl>
2022-09-12 18:29:59 +0200

    Introducing a pwm out vibration motor - more granular control of vibration patterns

--- creature/lib/components/vibration_motor_digital.py
+++ creature/lib/components/vibration_motor_digital.py
@@ -1,14 +1,13 @@
 # vibration motor
 
-import time
 import board
-import pwmio
+import digitalio
 
 class VibrationMotor():
     def __init__(self):
-        self.motor = pwmio.PWMOut(board.D2, frequency=300, duty_cycle=0)
+        self.motor = digitalio.DigitalInOut(board.D2)
         self.motor.direction = digitalio.Direction.OUTPUT
 
     # Takes either true or false
     def update(self, value):
-        self.motor.duty_cycle = value
+        self.motor.value = value
